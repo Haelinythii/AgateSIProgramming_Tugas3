bccc4647a447e033e7bbfe2ad734da36e7184109
Haelinythii <dwikiiskandar8@gmail.com>
2021-09-15 21:11:28 +0800

    Game Build

--- 2D Endless Runner/Assets/Scripts/Bird.cs
+++ 2D Endless Runner/Assets/Scripts/Bird.cs
@@ -7,13 +7,16 @@ public class Bird : MonoBehaviour
     float speed = 2f;
     private void Update()
     {
+        //gerak ke kiri terus menerus
         transform.position -= new Vector3(speed * Time.deltaTime, 0f);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //kalau collide sama bird destroyer
         if(collision.tag == "BirdDestroyer")
         {
+            //matikan gameobject
             gameObject.SetActive(false);
         }
     }
